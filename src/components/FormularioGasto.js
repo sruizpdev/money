5ef950661156803e40c240ef164b39ff4f437f64
sergiotto <sergiotto@outlook.com>
2023-01-18 12:32:08 +0100

    picker

--- src/components/FormularioGasto.js
+++ src/components/FormularioGasto.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  View,
+  TextInput,
+  Pressable,
+  StyleSheet,
+} from 'react-native';
 
 const FormularioGasto = () => {
   return (
     <SafeAreaView>
-      <Text>FormularioGasto</Text>
+      <View>
+        <Pressable>
+          <Text>Cancelar</Text>
+        </Pressable>
+      </View>
+      <View>
+        <Text>Nuevo Gasto</Text>
+        <View>
+          <Text>Nombre del gasto</Text>
+          <TextInput placeholder="Introduce el nombre del gasto. Ej. Comida" />
+        </View>
+        <View>
+          <Text>Cantidad del gasto</Text>
+          <TextInput
+            placeholder="Introduce la cantidad del gasto. Ej. 300"
+            keyboardType="numeric"
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
+
+const style = StyleSheet.create({});
 
 export default FormularioGasto;
